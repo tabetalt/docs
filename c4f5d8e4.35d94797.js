2de970fc227cc12f66f536b33f393015cce41903
cobraz <cobraz@cobraz.no>
2020-06-28 11:13:46 +0000

    Deploying to gh-pages from  @ 5317942c682552ad68398428c8bc27884d6d1538 🚀

---
+++ c4f5d8e4.35d94797.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[9],{148:function(n,e,t){"use strict";t.r(e);var a=t(0),c=t.n(a),o=t(164),i=t(154);e.default=function(){return c.a.createElement(o.a,{to:Object(i.a)("/index")})}}}]);
