bd3759e1d2a2d4ca25e70c857e9c5d7390ca9d91
Simen A. W. Olsen <cobraz@cobraz.no>
2020-10-11 19:04:35 +0200

    fix: Add roadmap to title

--- src/pages/roadmap.js
+++ src/pages/roadmap.js
@@ -5,7 +5,7 @@ import Layout from '@theme/Layout';
 
 const Roadmap = () => {
   return (
-    <Layout title="Hello">
+    <Layout title="Roadmap">
       
       <Iframe
         url="https://portal.productboard.com/tabetalt/1-tabetalt-product-portal/tabs/1-under-consideration?hide_header=1"
